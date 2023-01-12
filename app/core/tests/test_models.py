4a186ee01f9ec63946140bd40f7932c86898e742
sobhan <sobhanmoghimi45@gmail.com>
2023-01-12 18:26:57 +0330

    Completed User Model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,18 @@ class ModelTest(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'password123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test if creating a user without an email raises an error."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'testpass123')
+
+    def test_create_superuser(self):
+        """Test creating a new superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
